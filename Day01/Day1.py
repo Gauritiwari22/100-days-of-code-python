48d5d3c1ca78ae0e93cf86a95975d0d3c3427dbb
Gauritiwari22 <gauritiwari022@gmail.com>
2025-12-26 16:15:41 +0530

    Day 1: Basic functions and variables

---
+++ Day01/Day1.py
@@ -0,0 +1,23 @@
+"Day1"
+
+# Print function- used to print conetnt when code is run
+print("hello world")
+print("hello world\nhello world\nhello world")
+
+# String concatenation- adding/joining two strings
+print("Hello "+ "Gauri")
+
+# Input function- takes user input
+input("enter your name:")
+
+print("Hello,"+ input("enter your name:")+ "!")
+
+# Variables- contain data in them
+name="Richard"
+print(name)
+# here name is a variable containing the value Richard
+
+# Len() function- counts the total elements present in a variable/dataset, counts spaces as well
+print(len(name))
+
+
